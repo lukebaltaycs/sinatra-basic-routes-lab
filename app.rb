002bd8845664bdea037c028e2262dc14acbbc4c4
Luke Baltay <luke.baltay@yale.edu>
2022-06-21 13:17:28 -0400

    commit tree d86b9ffbffc9c61fbe49d4607ec8bb3620e70f8b

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Luke Baltay"
+    end
+
+    get '/hometown' do
+        "My hometown is Milton, Massachuessetts"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Never Meant' by 'American Football'"
+    end
+
 end
